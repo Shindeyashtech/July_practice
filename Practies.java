4fb0fd5fb445933f2e6037a379a31db5191abf2c
Yash shinde <yashshinde.tech@gmail.com>
2025-08-10 16:52:48 +0000

    Add Practies class with basic arithmetic and bitwise operations

--- Practies.java
+++ Practies.java
@@ -5,7 +5,16 @@ class Practies{
                 System.out.println(++a);
                 System.out.println(--a);
                 System.out.println(a--);
-                
+                int b =50;
+                System.out.println(a+b);
+                System.out.println(a-b);
+                System.out.println(a*b);
+                System.out.println(a/b);
+                System.out.println(a%b);
+                int c =10;
+                System.out.println(20<<2);
+                System.out.println(20>>2);
+                System.out.println(a<b&a<c);             
 
                 }
 }
