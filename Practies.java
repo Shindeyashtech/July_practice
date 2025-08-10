dc096a2ed7358d73808fbba81f7e3d6a1532d3b1
Yash shinde <yashshinde.tech@gmail.com>
2025-08-10 16:40:14 +0000

    Practies

---
+++ Practies.java
@@ -0,0 +1,11 @@
+class Practies{
+               public static void main(String[] args) {
+                int a=20;
+                System.out.println(a++);
+                System.out.println(++a);
+                System.out.println(--a);
+                System.out.println(a--);
+                
+
+                }
+}
