b4569eb20b9687b426db43eaf2c215b43b0d680a
Yash shinde <yashshinde.tech@gmail.com>
2025-08-10 15:35:14 +0000

    Update

---
+++ Add.java
@@ -0,0 +1,14 @@
+class Add{
+    public static void main(String[] args) {
+        System.out.println("Addition of Two Number");
+        int a,b;
+        a=20;
+        b=10;
+        int res=a+b;
+        int res1=a-b;
+        int res2=a/b;
+        int res3=a%b;
+        int res4=a*b;
+        System.out.println(res+" "+ res1+" "+res2+" "+res3+" "+res4+"  " );     
+    }
+}
