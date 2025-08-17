2b6ee7be0057dbbfd0856d433d144a264217602d
Yash shinde <yashshinde.tech@gmail.com>
2025-08-17 17:19:17 +0000

    Add Pyramid class to print a star pyramid pattern

---
+++ Pyramid.java
@@ -0,0 +1,15 @@
+public class Pyramid {
+
+    public static void main(String[] args) {
+        System.out.println(" Star pyramid");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <=i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
