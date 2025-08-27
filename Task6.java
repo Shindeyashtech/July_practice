cd267f711d4c14a2f3a083e6c2d36d0ec0580b17
Yash shinde <yashshinde.tech@gmail.com>
2025-08-27 18:34:01 +0000

    Add Task6 class to print numbers 1–50 and even numbers using different loop types

---
+++ Task6.java
@@ -0,0 +1,33 @@
+// Print numbers 1–50 using a for loop.
+
+// Print even numbers 1–50 using a while loop.
+
+// Print the multiplication table of any number using a do-while loop.
+
+
+// Focus: difference between loop types.
+class NumberPrint{
+    public int i;
+   public  int forLoop(){
+        for ( i = 0; i < 50; i++) {
+            System.out.println(i);
+        }
+        return i;
+    }
+    public int WhileLoop(){
+        while (i<=50) {
+            if (i%2==0) {
+                System.out.println(i);
+            }
+        }
+     return i; 
+    }
+}
+
+public class Task6 {
+    public static void main(String[] args) {
+NumberPrint n1=new NumberPrint();
+n1.forLoop();
+n1.WhileLoop();
+     }
+}
