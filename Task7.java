84b11661594fb85fcb67f88dc43824c0daeac283
Yash shinde <yashshinde.tech@gmail.com>
2025-08-30 17:39:33 +0000

    Add Task7 class to demonstrate object creation and field access for Student objects

---
+++ Task7.java
@@ -0,0 +1,13 @@
+// Task 7: Class & Object
+
+// Create a class Student with variables name, age, and marks.
+
+// In main, create 3 students and print their info.
+
+
+// Focus: object creation, accessing fields.
+
+
+public class Task7 {
+    
+}
