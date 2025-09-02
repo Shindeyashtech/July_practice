5f1f6be9c9d89bc73b7f93fdf7ba317bf8085d25
Yash shinde <yashshinde.tech@gmail.com>
2025-09-02 18:50:35 +0000

    Add MathOps class with method overloading for addition

---
+++ Task8.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+// Task 8: Methods & Method Overloading
+// Write a class MathOps with methods:
+// add(int, int)
+// add(double, double)
+// add(int, int, int)
+// Call them from main and print results.
+// Focus: methods, method overloading, reusability.
+class MathOps{
+     public int Add(int a,int b){
+        return 0;
+     }
+     public int Add(double  a,double  b){
+        return 0;
+     }
+     public int Add(int a,int b,int c){
+        return 0;
+     }
+ 
+}
+
+public class Task8 {
+public static void main(String[] args) {
+
+    MathOps m1=new MathOps();
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter Number1");
+int
+
+      m1.Add(a, b);
+    m1.Add(a, b);
+    m1.Add(a, b, c);
+
+}
+    
+}
