c417641729e221cda098c033f6934f6e0ecdb080
Yash shinde <yashshinde.tech@gmail.com>
2025-09-04 17:35:17 +0000

    Refactor Task10.java to correctly initialize and print the 3x3 matrix values

--- Task10.java
+++ Task10.java
@@ -8,11 +8,19 @@
 public class Task10 {
     public static void main(String[] args) {
         int num[][]= new int[3][3];  
+         
+
          for (int i = 0; i < num.length; i++) {
               for (int j = 0; j < num.length; j++) {
-                  System.out.print(num[i][j]);
+                num[i][j]= (int)(Math.random()*100);
+
+            }
+           }      
+         for (int i = 0; i < num.length; i++) {
+              for (int j = 0; j < num.length; j++) {
+                  System.out.print(num[i][j]+ " ");
               }
               System.out.println(" ");
-         }      
+          }      
     }
 }
