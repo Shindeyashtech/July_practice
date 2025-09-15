d6e72637e38756810635d5f385281aa306345e82
Yash shinde <yashshinde.tech@gmail.com>
2025-09-15 17:36:54 +0000

    Add AryObj.java to implement a basic student management system with a Student class

---
+++ AryObj.java
@@ -0,0 +1,28 @@
+ class Student{
+    int roll;
+    int mark;
+    String name;
+
+ }
+public class AryObj{
+
+    public static void main(String[] args) {
+      
+         Student s1=new Student();
+         s1.name="y";
+         s1.mark=44;
+         
+         Student s2=new Student();
+         s2.name="ash";
+         s2.mark=4;
+         Student s3=new Student();
+         s3.name="yash";
+         s3.mark=44;
+
+         Student stud[]=new Student[5];
+         for (int i = 0; i <stud.length; i++) {
+            
+             
+         }
+    }
+}
