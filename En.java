278429b235ee2337759bba376d9eea9ac8e5f180
Yash shinde <yashshinde.tech@gmail.com>
2025-09-18 02:56:18 +0000

    Add En.java class with main method and array initialization

---
+++ En.java
@@ -0,0 +1,17 @@
+public class En {
+    public static void main(String[] args) {
+
+        int num[] =new int [6]; 
+        num[0]=0;
+        num[1]=1;
+        num[2]=2;
+        num[3]=3;
+        num[4]=4;
+        num[5]=5;
+        for(int n : num){
+            System.out.println(num[n]+" "+n);
+        }
+    }}
+
+        
+          
